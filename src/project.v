2231ff952093a2a16805a72a7b39205bb82e506a
lilly <ldas@andrew.cmu.edu>
2026-02-21 12:43:27 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_example_sillylad (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
+  assign uio_out[7:1] = '0;
   assign uio_oe  = 8'b0000_0001;
 
   // List all unused inputs to prevent warnings
